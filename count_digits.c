7f9a698b84a1cae5863f8e897a953d4e86c8c6ef
Noble-neon <muminovsadullo777@gmail.com>
2026-09-18 12:32:26 +0500

    small changes to count_digits

--- count_digits.c
+++ count_digits.c
@@ -2,11 +2,11 @@
 
 int main(void) {
     int a;
-    scanf('%i', &a);
+    scanf("%i", &a);
     int n = 0;
     while (a>0){
         a = a/10;
         n++;
     }
-    print('%i', n);
+    printf("%i", n);
 }
