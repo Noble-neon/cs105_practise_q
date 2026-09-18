5e8af9401d1071bc3be3b60781bfc28936d29e8c
Noble-neon <muminovsadullo777@gmail.com>
2026-09-18 12:38:51 +0500

    modified count_digits for better output

--- count_digits.c
+++ count_digits.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 int main(void) {
-    int a;
-    scanf("%i", &a);
+    long long a;
+    scanf("%lli", &a);
+    long long copy = a;
     int n = 0;
     while (a>0){
         a = a/10;
         n++;
     }
-    printf("%i", n);
+    printf("the number %lli has %i digits", copy, n);
 }
